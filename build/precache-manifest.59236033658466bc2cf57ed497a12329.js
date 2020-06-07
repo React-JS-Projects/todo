60ae10c2c4fcb0cedd89211f2b346e8dda6e37d6
Visakh Vijayan <visakh.vijayan@seenit.in>
2020-06-07 21:48:04 +0530

    adding build

---
+++ build/precache-manifest.59236033658466bc2cf57ed497a12329.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "326e3e3206dae67d139c234fb0d9671f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "208c5cc04434c4ddc60f",
+    "url": "/static/css/main.7b9dd2b1.chunk.css"
+  },
+  {
+    "revision": "1112ae84c5209b9a7850",
+    "url": "/static/js/2.04b47026.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.04b47026.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "208c5cc04434c4ddc60f",
+    "url": "/static/js/main.e595c5d4.chunk.js"
+  },
+  {
+    "revision": "759e81ee1e3f967c5c66",
+    "url": "/static/js/runtime-main.66c35898.js"
+  }
+]);
